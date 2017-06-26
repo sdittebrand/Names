2c71be90313190a7ae75eab6bb51c3e991096a4f
Seth Dittebrand <sdittebrand@gmail.com>
2017-06-26 13:40:13 -0400

    removed redundant code

--- names.rb
+++ names.rb
@@ -1,11 +1,8 @@
 print "Please enter your first name:"
-fname = gets
-fname = fname.chomp
+fname = gets.chomp
 print "Please enter your last name:"
-lname = gets
-lname = lname.chomp
+lname = gets.chomp
 puts "Hello #{fname} #{lname}, I hope you're having a good day!"
 print "So, how old are you, #{fname}?"
-age = gets
-age = age.chomp
+age = gets.chomp
 print "You're #{age} years old? Lookin' good!"
