62b1929a4434037c5620d1fb70e831aa7741c6b3
Seth Dittebrand <sdittebrand@gmail.com>
2017-06-26 13:37:41 -0400

    asks for the users age

--- names.rb
+++ names.rb
@@ -4,4 +4,8 @@ fname = fname.chomp
 print "Please enter your last name:"
 lname = gets
 lname = lname.chomp
-print "Hello #{fname} #{lname}, I hope you're having a good day!"
+puts "Hello #{fname} #{lname}, I hope you're having a good day!"
+print "So, how old are you, #{fname}?"
+age = gets
+age = age.chomp
+print "You're #{age} years old? Lookin' good!"
