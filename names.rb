a65f773444431b276123e223734773f336e1c5be
Seth Dittebrand <sdittebrand@gmail.com>
2017-06-26 11:44:49 -0400

    used .chomp to fix line errors

--- names.rb
+++ names.rb
@@ -1,5 +1,7 @@
 print "Please enter your first name:"
 fname = gets
+fname = fname.chomp
 print "Please enter your last name:"
 lname = gets
+lname = lname.chomp
 print "Hello #{fname} #{lname}, I hope you're having a good day!"
