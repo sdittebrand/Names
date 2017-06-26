38d869cf22c8fa2abf2e8dd5e61af18d30a5cdc6
Seth Dittebrand <sdittebrand@gmail.com>
2017-06-26 14:21:03 -0400

    changed to use functions for each part

--- names.rb
+++ names.rb
@@ -1,8 +1,20 @@
-print "Please enter your first name:"
-fname = gets.chomp
-print "Please enter your last name:"
-lname = gets.chomp
-puts "Hello #{fname} #{lname}, I hope you're having a good day!"
-print "So, how old are you, #{fname}?"
-age = gets.chomp
-print "You're #{age} years old? Lookin' good!"
+#print "Please enter your first name:"
+#fname = gets.chomp
+#print "Please enter your last name:"
+#lname = gets.chomp
+#puts "Hello #{fname} #{lname}, I hope you're having a good day!"
+#print "So, how old are you, #{fname}?"
+#age = gets.chomp
+#print "You're #{age} years old? Lookin' good!"
+
+def first_name()
+    print "What's your first name?"
+    fname = gets.chomp
+end
+
+def last_name()
+    print "What's your last name?"
+    lname = gets.chomp
+end
+
+print "Hello #{fname} #{lname}, how are you doing?"
